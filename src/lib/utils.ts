40654fd0280c7806ac1e7bb43ed98cfa97920836
Prathamesh0009M <prathameshj776@gmail.com>
2025-04-09 18:28:32 +0530

    Fix tsconfig & utils.ts warnings + build errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+// import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: (string | undefined | null | false)[]) {
   return inputs.filter(Boolean).join(" ")
